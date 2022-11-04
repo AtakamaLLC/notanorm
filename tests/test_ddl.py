6656b26e6d48e16c5c49510201874a5eb4819da3
erik aronesty <erik@q32.com>
2022-11-04 16:15:22 -0400

    linter

--- tests/test_ddl.py
+++ tests/test_ddl.py
@@ -1,13 +1,15 @@
 import sys
 import logging
+import pytest
 from notanorm import DbModel, DbCol, DbType, DbTable, DbIndex
 
 from notanorm.ddl_helper import model_from_ddl
 
 log = logging.getLogger(__name__)
 
-if tuple(sys.version_info[0:2]) <= (3,6):
-  pytest.skip("sqlglot requires python 3.7 or greateer")
+if tuple(sys.version_info[0: 2]) <= (3, 6):
+    pytest.skip("sqlglot requires python 3.7 or greateer")
+
 
 def test_model_ddl_cap(db):
     # creating a model using sqlite results in a model that generally works across other db's
