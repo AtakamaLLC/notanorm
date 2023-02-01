2923d18ba81bbe9edd435038aff48891c391c2f0
erik aronesty <erik@q32.com>
2023-02-01 09:54:50 -0500

    comment on dunder

--- notanorm/model.py
+++ notanorm/model.py
@@ -93,7 +93,9 @@ class DbIndex(NamedTuple):
     fields: Tuple[DbIndexField, ...]
     unique: bool = False  # has a unique index?
     primary: bool = False  # is the primary key?
-    ix__name: Optional[str] = None  # only avail when reading from a db model, not a ddl
+
+    # don't use this field, it's internal, but named tuples are weirdly limited
+    ix__name: Optional[str] = None
 
     def _as_tup(self) -> Tuple[Tuple[DbIndexField, ...], bool, bool]:
         return (self.fields, self.unique, self.primary)
@@ -104,8 +106,9 @@ class DbIndex(NamedTuple):
     def __hash__(self):
         return hash(self._as_tup())
 
+    # correct way to access the name
     @property
-    def name(self):
+    def name(self) -> Optional[str]:
         return self.ix__name
 
 
