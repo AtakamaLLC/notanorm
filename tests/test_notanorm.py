e8787287fe32d66fd2285ec63dfaef5345c6ac88
erik aronesty <erik@q32.com>
2023-03-17 16:49:45 -0400

    cover

--- tests/test_notanorm.py
+++ tests/test_notanorm.py
@@ -378,9 +378,10 @@ def test_db_upsert(db_sqlup):
 
 
 def test_db_insert_no_vals(db):
-    db.query("create table foo (bar integer default 1)")
+    db.query("create table foo (bar integer default 1, baz double default 2.2)")
     db.insert("foo")
     assert db.select_one("foo").bar == 1
+    assert db.select_one("foo").baz == 2.2
 
 
 def test_db_insert_lrid(db):
