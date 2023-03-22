60f049f67d1fdb68c33cda548abda52e539ac06d
erik aronesty <erik@q32.com>
2023-03-22 11:24:39 -0400

    removea1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def long_description():
 
 setup(
     name="notanorm",
-    version="3.9.1a5",
+    version="3.9.1",
     description="DB wrapper library",
     packages=["notanorm"],
     url="https://github.com/AtakamaLLC/notanorm",
